e1093ca7b4e5e5a550bdd87af0dd73ef6997d068
yoon1217 <fransisco12@naver.com>
2018-03-13 11:03:08 +0900

    message

---
+++ src/homework1/Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    public static void main(String...args) {
+        System.out.printf("%d+%d=%d\n", 1, 2, 1 + 2);
+    }
+    }
---
+++ src/homework1/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main (String...args) {
+        //print HelloWorld
+        System.out.println("Hello,World!");
+    }
+}
